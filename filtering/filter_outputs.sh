29b6579fb5758faab6e01d5e4e02ee569d8c59b8
namsyvo <namsyvo@gmail.com>
2018-06-20 23:41:58 +0000

    Add scripts to do filtering and re-annotation for PureCN results

---
+++ filtering/filter_outputs.sh
@@ -0,0 +1,1 @@
+cwltool --debug --tmpdir-prefix /mnt/tmp/ --tmp-outdir-prefix /mnt/tmp/ --outdir /mnt/data/DLBC37/PureCN_cwl/tumor_varcall complement_outputs.cwl complement_outputs.yaml
---
+++ filtering/merge_vcfs.sh
@@ -0,0 +1,1 @@
+cwltool --debug --tmpdir-prefix /mnt/tmp/ --tmp-outdir-prefix /mnt/tmp/ --outdir /mnt/data/DLBC37/PureCN_cwl/tumor_varcall_test2/ merge_vcfs.cwl merge_vcfs.yaml 2> /mnt/data/DLBC37/PureCN_cwl/tumor_varcall_test2/merge_vcfs.log &
